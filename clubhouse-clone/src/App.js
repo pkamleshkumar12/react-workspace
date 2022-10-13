c18c6a2bbb0994196791ecfd1d0754d5d8567f47
kamlesh <pkamleshkumar12@gmail.com>
2022-10-13 22:54:34 +0800

    Notification and PhoneConfirmation pages

--- clubhouse-clone/src/App.js
+++ clubhouse-clone/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './pages/Welcome';
+import "./App.css";
+import Welcome from "./pages/Welcome";
+import PlanLayout from "./pages/Layout/PlanLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PhoneConfirmation from "./pages/PhoneConfirmation";
+import CodeConfirm from "./pages/CodeConfirm";
+import AllowNotification from "./pages/AllowNotification";
+
 
 function App() {
   return (
-     <Welcome></Welcome>
+    <BrowserRouter>
+      <PlanLayout>
+        <Routes>
+          <Route exact path='/' element={<Welcome/>} />
+          <Route exact path='/invite' element={<PhoneConfirmation/>} />
+          <Route exact path='/code_confirm' element={<CodeConfirm/>} />
+          <Route exact path='/allow_notification' element={<AllowNotification/>} />
+
+        </Routes>
+      </PlanLayout>
+    </BrowserRouter>
   );
 }
 
--- clubhouse-clone/src/pages/Welcome.js
+++ clubhouse-clone/src/pages/Welcome.js
@@ -1,5 +1,7 @@
 import React from "react";
 import style from '../style/welcome.module.css';
+import { Link } from "react-router-dom";
+
 
 export default function Welcome(){
     return (
@@ -10,11 +12,19 @@ export default function Welcome(){
                     We're working hard to get the clubhouse for everyone!
                     While we wrap up the finishing touches, we're adding people gradually to make sure nothing breaks
                 </p>
-
                 <p>
-                    Anyone can join with an invite from an existing user or 
+                    Anyone can join with an invite from an existing user or reserve your username and we'll text you if you have a friend on the app who can let you in. We are so gratefull you're here and can't wait to have you join!
                 </p>
-
+                <p>
+                    Kamlesh & the Clubhouse team
+                </p>
+            </div>
+            <div className={style.actionBtn}>
+                <Link exact to='/get_username' className='primaryBtn d-flex align-items-center mb-3'>
+                   Get your username{" "}
+                   {/* <img src="" alt=""/> */}
+                </Link>
+                <Link>Have an invite text? Sign in</Link>
             </div>
         </div>
 
